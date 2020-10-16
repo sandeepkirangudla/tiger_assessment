4301ccad0fe16e67a3b18d9603e4bca849a8a1f0
gsand <gsandeepkiran@gmail.com>
2020-10-15 19:49:01 -0500

    final edit

--- config.py
+++
@@ -1,8 +0,0 @@
-config = {
-    "collision": "chicago_collision_data.json",
-    "flight_call":"flight_call.json",
-    "light_level": "light_levels.json",
-    "def_input_path": ".\Data\Input",
-	"def_output_path": ".\Data\Output",
-	"log_path": ".\Logs"
-}
